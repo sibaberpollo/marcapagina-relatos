73a24c82978d8ba4f31c37198600a77e493e4658
José Pino <jose.pino@tvn.cl>
2025-04-22 10:00:36 -0400

    Ajustados avatares

--- components/Card.tsx
+++ components/Card.tsx
@@ -18,7 +18,30 @@ const Card = ({ title, description, imgSrc, href, authorImgSrc }: CardProps) => 
     >
       {imgSrc &&
         (href ? (
-          <Link href={href} aria-label={`Link to ${title}`}>
+          <div className="relative">
+            <Link href={href} aria-label={`Link to ${title}`}>
+              <Image
+                alt={title}
+                src={imgSrc}
+                className="object-cover object-center md:h-36 lg:h-48"
+                width={544}
+                height={306}
+              />
+            </Link>
+            {authorImgSrc && (
+              <div className="absolute left-1/2 bottom-0 transform -translate-x-1/2 translate-y-1/2 z-10">
+                <Image
+                  alt="Autor"
+                  src={authorImgSrc}
+                  className="rounded-full border-4 border-white dark:border-gray-900"
+                  width={80}
+                  height={80}
+                />
+              </div>
+            )}
+          </div>
+        ) : (
+          <div className="relative">
             <Image
               alt={title}
               src={imgSrc}
@@ -26,29 +49,21 @@ const Card = ({ title, description, imgSrc, href, authorImgSrc }: CardProps) => 
               width={544}
               height={306}
             />
-          </Link>
-        ) : (
-          <Image
-            alt={title}
-            src={imgSrc}
-            className="object-cover object-center md:h-36 lg:h-48"
-            width={544}
-            height={306}
-          />
+            {authorImgSrc && (
+              <div className="absolute left-1/2 bottom-0 transform -translate-x-1/2 translate-y-1/2 z-10">
+                <Image
+                  alt="Autor"
+                  src={authorImgSrc}
+                  className="rounded-full border-4 border-white dark:border-gray-900"
+                  width={80}
+                  height={80}
+                />
+              </div>
+            )}
+          </div>
         ))}
       
-      <div className="p-6">
-        {authorImgSrc && (
-          <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <Image
-              alt="Autor"
-              src={authorImgSrc}
-              className="rounded-full border-4 border-white dark:border-gray-900"
-              width={80}
-              height={80}
-            />
-          </div>
-        )}
+      <div className={`p-6 ${authorImgSrc ? 'pt-10' : ''}`}>
         <h2 className={`mb-3 text-2xl leading-8 font-bold tracking-tight ${authorImgSrc ? 'mt-10' : ''}`}>
           {href ? (
             <Link href={href} aria-label={`Link to ${title}`}>
