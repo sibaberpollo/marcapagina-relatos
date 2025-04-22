7e143925ddb2c60148f5020b18f39481dff4d857
José Pino <jose.pino@tvn.cl>
2025-04-22 14:25:14 -0400

    Ajustes metadata, favicon

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
             Relatos
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Escribir de cuando en vez
+            Gente que escribe de cuando en vez
           </p>
         </div>
         <div className="container py-12">
