43177fc4a7d4189560de73b2eaa0a8c9ca2ee723
José Pino <jose.pino@tvn.cl>
2025-04-24 17:41:59 -0400

    Ajustes de margins

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -42,7 +42,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
     <SectionContainer>
       <ScrollTopAndComment />
       <article>
-        <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
+        <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700 mb-10">
           <header className="pt-6 xl:pb-6">
             <div className="space-y-1 text-center">
               <dl className="space-y-10">
