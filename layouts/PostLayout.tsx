735dffd95b4682a485572aa46d1fb703291c844e
José Pino <jose.pino@tvn.cl>
2025-04-26 18:47:04 -0400

    mejorados espacios en móvil para el layout del relato

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -85,7 +85,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
           </header>
 
           <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0 dark:divide-gray-700">
-            <dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
+            <dl className="hidden md:flex pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
               <dt className="sr-only">Authors</dt>
               <dd>
                 <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:block xl:space-y-8 xl:space-x-0">
@@ -131,7 +131,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             </dl>
 
             <div className="divide-y divide-gray-200 xl:col-span-3 xl:row-span-2 xl:pb-0 dark:divide-gray-700">
-              <div className="prose dark:prose-invert max-w-none pt-10 pb-8">{children}</div>
+              <div className="prose dark:prose-invert max-w-none pt-2 pb-8 xl:pt-10">{children}</div>
             </div>
 
             <footer>
