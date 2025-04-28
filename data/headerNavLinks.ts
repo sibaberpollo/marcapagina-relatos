4589ecd636279b23f03e115d8dc9c55bc8f4a9dc
José Pino <jose.pino@tvn.cl>
2025-04-28 17:01:23 -0400

    logo and navLinks

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Inicio' },
-  { href: '/autor/anairene', title: '' },
-  { href: '/autor/hazael', title: '' },
-  { href: '/autor/pino', title: '' },
+  { href: '/acerca-de', title: 'Acerca de' }
 ]
 
 export default headerNavLinks
