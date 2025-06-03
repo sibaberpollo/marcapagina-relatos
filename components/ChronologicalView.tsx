aca0395f65c4088b818cd6ecfb3e77442aec0204
José Pino <pino.jose@gmail.com>
2025-06-03 15:32:15 -0400

    Add loading spinner for Cronologico route and simplify view

--- components/ChronologicalView.tsx
+++ components/ChronologicalView.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
 import ChronologicalLayout from '@/layouts/ChronologicalLayout'
-import LoadingSpinner from './LoadingSpinner'
 
 interface ChronologicalViewProps {
   items: any[]
@@ -11,26 +9,11 @@ interface ChronologicalViewProps {
 }
 
 export default function ChronologicalView({ items, itemsPerPage, currentPage }: ChronologicalViewProps) {
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    setIsLoading(true)
-    const timer = setTimeout(() => {
-      setIsLoading(false)
-    }, 500) // Pequeño delay para asegurar que el spinner se muestre
-
-    return () => clearTimeout(timer)
-  }, [currentPage])
-
-  if (isLoading) {
-    return <LoadingSpinner />
-  }
-
   return (
-    <ChronologicalLayout 
+    <ChronologicalLayout
       items={items}
       itemsPerPage={itemsPerPage}
       currentPage={currentPage}
     />
   )
-} 
+}
