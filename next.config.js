d5660208f755a70cb8589addd55d1608b6a42381
José Pino <pino.jose@gmail.com>
2025-06-20 18:54:57 -0400

    Permitir frames de Google CSE

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is https://challenges.cloudflare.com https://www.googletagmanager.com https://cse.google.com;
   script-src-elem 'self' 'unsafe-inline' https://challenges.cloudflare.com https://www.googletagmanager.com https://cse.google.com;
-  frame-src 'self' https://challenges.cloudflare.com https://www.youtube.com https://open.spotify.com;
+  frame-src 'self' https://challenges.cloudflare.com https://www.youtube.com https://open.spotify.com https://cse.google.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
