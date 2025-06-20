ed0a63c5f492891d863d0c85f98c8d0ca649f6de
José Pino <pino.jose@gmail.com>
2025-06-20 18:49:32 -0400

    Usar form para enviar busquedas

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,13 +4,14 @@ import { Search } from 'lucide-react'
 
 export default function SearchBar({ className = '' }: { className?: string }) {
   return (
-    <div className={`relative ${className}`}>
+    <form action="/resultados" method="get" className={`relative ${className}`}> 
       <input
         type="text"
+        name="q"
         placeholder="Buscar..."
         className="w-full border border-black bg-gray-100 rounded pl-3 pr-8 py-1 text-sm focus:outline-none"
       />
-      <Search className="absolute right-2 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-600" />
-    </div>
+      <Search className="pointer-events-none absolute right-2 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-600" />
+    </form>
   )
 }
