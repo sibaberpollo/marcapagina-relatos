ee71968a93590a1f75a8b2226b2cff06147dce13
José Pino <pino.jose@gmail.com>
2025-06-20 18:42:38 -0400

    Agregar página de resultados y ajustar CSP

---
+++ app/resultados/page.tsx
@@ -0,0 +1,21 @@
+import SectionContainer from '@/components/SectionContainer'
+import PageTitle from '@/components/PageTitle'
+import { genPageMetadata } from 'app/seo'
+import Script from 'next/script'
+
+export const metadata = genPageMetadata({
+  title: 'Resultados de búsqueda',
+  description: 'Explora el contenido de Marcapágina a través de Google.'
+})
+
+export default function ResultadosPage() {
+  return (
+    <SectionContainer>
+      <div className="py-8">
+        <PageTitle>Resultados de búsqueda</PageTitle>
+        <Script async src="https://cse.google.com/cse.js?cx=20d090db2c0284144" />
+        <div className="gcse-search" />
+      </div>
+    </SectionContainer>
+  )
+}
