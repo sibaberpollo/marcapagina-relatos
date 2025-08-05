8f31b9acff281747254afb102dc2d73ecaed2263
José Pino <josepino@MacBook-Pro-de-Jose.local>
2025-08-05 08:36:21 -0400

    cambiar todo a myúsculas

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,5 @@
 export function toVersal(text: string): string {
   if (!text) return ''
   
-  return text
-    .split(' ')
-    .map(word => {
-      if (word.length === 0) return word
-      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-    })
-    .join(' ')
+  return text.toUpperCase()
 } 
