fd101d6207c2cad8177fb6198fde7c31dd18ba36
José Pino <josepino@MacBook-Pro-de-Jose.local>
2025-08-09 09:12:44 -0400

    fix ruta del auth

--- app/api/whoami/route.ts
+++ app/api/whoami/route.ts
@@ -1,7 +1,8 @@
-import { auth } from '@/auth'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../../../auth'
 
 export async function GET() {
-  const session = await auth()
+  const session = await getServerSession(authOptions)
   return Response.json({ session })
 }
 
